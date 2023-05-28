dd24abf8aa8ad2b681855225c5498b0479cb1ab3
87taw <tiago.bonito44@hotmail.com>
2023-05-28 18:53:03 -0300

    !jsd

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // http://localhost:5000/files
-app.use("/files") 
-express.static("uploads");
+app.use("/files", express.static("uploads")) 
+
 
 const db = require("./config/mongoose");
 app.set("Mongoose", db);
